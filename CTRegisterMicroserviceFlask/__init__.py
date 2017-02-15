54539de0304b77c5e9775894ae039aa77299e8db
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-02-15 18:46:10 +0100

    v.0.2.0

--- CTRegisterMicroserviceFlask/__init__.py
+++ CTRegisterMicroserviceFlask/__init__.py
@@ -7,21 +7,49 @@ AUTOREGISTER_MODE = 'AUTOREGISTER_MODE'
 NORMAL_MODE = 'NORMAL_MODE'
 
 def autoregister(app, name, info, swagger, mode, ct_url=False, url=False, active=True):
-  payload = {'name': name, 'url': url, 'active': active}
-  r = requests.post(ct_url+'/api/v1/microservice', json=payload)
-  if r.status_code != 200:
-    raise ValueError('microservice has not been registered')
+    """Autoregister method"""
+    payload = {'name': name, 'url': url, 'active': active}
+
+    try:
+        r = requests.post(ct_url+'/api/v1/microservice', json=payload)
+    except Exception as error:
+        raise Exception('Generic error')
+
+    if r.status_code != 200:
+        raise ValueError('Microservice has not been registered')
 
 def register(app, name, info, swagger, mode, ct_url=False, url=False, active=True):
-  if mode == AUTOREGISTER_MODE:
-    t = threading.Timer(5.0, autoregister, [app, name, info, swagger, mode, ct_url, url, active])
-    t.start()
+    """Register method"""
+    if mode == AUTOREGISTER_MODE:
+        t = threading.Timer(5.0, autoregister, [app, name, info, swagger, mode, ct_url, url, active])
+        t.start()
 
-  @app.route('/info')
-  def get_info():
-    info['swagger'] = swagger
-    return jsonify(info) #@TODO: JSONAPI format
+    @app.route('/info')
+    def get_info():
+        info['swagger'] = swagger
+        return jsonify(info)
 
-  @app.route('/ping')
-  def get_ping():
-    return "pong"
+    @app.route('/ping')
+    def get_ping():
+        return 'pong'
+
+def request_to_microservice(config):
+    """Request to microservice method"""
+    try:
+        session = Session()
+        request = Request(
+                method=config.get('method'),
+                url=CT_URL + config.get('uri'),
+                headers={
+                    'content-type': 'application/json',
+                    'Authorization': 'Bearer '+CT_TOKEN
+                },
+                data=json.dumps(config.get('body'))
+            )
+        prepped = session.prepare_request(request)
+
+        response = session.send(prepped)
+    except Exception as error:
+       raise error
+
+    return response.json()
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# ct-register-microservice-python-flask
+# Microservice Registration in Python Flask
+
+## Usage
+
+### How to register a microservice to CT
+
+```python
+    info = load_config_json('register')
+    swagger = load_config_json('swagger')
+    CTRegisterMicroserviceFlask.register(
+        app = application,
+        name = '<microservice_name>',
+        info = info,
+        swagger = swagger,
+        mode = CTRegisterMicroserviceFlask.AUTOREGISTER_MODE if os.getenv('ENVIRONMENT') == 'dev' else CTRegisterMicroserviceFlask.NORMAL_MODE,
+        ct_url = os.getenv('CT_URL'),
+        url = os.getenv('LOCAL_URL')
+    )
+```
+### How to do a request to another microservice
+
+```python
+config = {
+    'uri': '/dataset/12332-123123-123123-11111',
+    'method': 'GET',
+}
+response = request_to_microservice(config)
+```
